0a6ed3b1704fec77fac6f5f1ef19d7ad584deac2
Felipe <felipecostazucco@gmail.com>
2021-12-20 01:07:01 -0300

    modificacoes

--- src/main/java/com/tendelfc/TendelfcApplication.java
+++ src/main/java/com/tendelfc/TendelfcApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class TendelfcApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
 		SpringApplication.run(TendelfcApplication.class, args);
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 	}
 
 }
