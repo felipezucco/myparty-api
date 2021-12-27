5fc72e620d146826e2b7584e84104cf925440d68
Felipe <felipecostazucco@gmail.com>
2021-12-26 22:55:26 -0300

    push sem muitas alterações

--- src/main/java/com/tendelfc/TendelfcApplication.java
+++ src/main/java/com/tendelfc/TendelfcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TendelfcApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(TendelfcApplication.class, args);
 	}
 
